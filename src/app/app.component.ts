9fa91d1b194daedf73d33f98f1cc52fcb0cc5339
Christopher Mattox <metroplex02@gmail.com>
2017-02-20 22:10:16 -0600

    found template in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+<h1>Hello from the {{componentName}}.</h1>
+`
 })
 export class AppComponent {
   title = 'Found The Title!';
@@ -11,8 +14,15 @@ export class AppComponent {
   image = 'http://vignette2.wikia.nocookie.net/disney/images/2/24/Captain_america_civil_war_promoart-spiderman.jpg/revision/latest?cb=20161017165547'
 
   clicked(event) {
-  console.log("Weeeee")
+    let someArray = [1, "string", false];
+      for (let entry of someArray) {
+
+        console.log(entry);
+      }
+      console.log("Weeeee")
   }
+
+
 
 
 }
